2aadaeeea737acb4755b7168dc0c378222c6adad
Ligia <ligia.j38@gmail.com>
2023-03-20 21:35:59 -0400

    ML

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 Este proyecto se divide en cuatro etapas, las cuales se muestran en el siguiente diagrama:
 
 <h1 align=center> PRIMERA PARTE DEL PROYECTO: ETL </h1>
-
-_Nota: Colocar el esta parte un esquema con lo que se realizó en cada una de las tres etapas. En la etapa de la carga de los datos se utilizaron los archivos .csv suministrados por la empresa. En la etapa de Transformación colocar en el esquema todas las transformaciones realizadas y por último en la etapa de la carga de los datos colocar que se juntó toda la data en un sólo dataframe y se exporto._ --> Co esto terminaria la parte introductoria y vendría la parte del código .
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #=================================================================================#
-#                                 FASTAPI                                         #
+#                    SEGUNDA PARTE DEL PROYECTO: FASTAPI                          #
 #=================================================================================#
 # libraries
 from fastapi import FastAPI
@@ -55,8 +55,52 @@ async def get_count_platform(platform):
 #---------------------------------------------------------------------------------#
 # get_actor function:
 @app.get('/get_actor/{platform}/{year}')
-async def get_actor(platform):
-    return 'string'
+async def get_actor(platform, year):
+    df_f1 = data[data['id'].str[0] == platform[0].lower()]
+    df_f2 = df_f1[df_f1['release_year'] == int(year)]
+    actors_serie = df_f2['cast']
+    # Remove the missing values in actors:
+    actors_serie_f = actors_serie.dropna()
+    # Pass the serie from Pandas to a Python list:
+    actors_list = actors_serie_f.to_numpy().tolist()
+    # Separate the actors using ', ' like separator
+    actors_list = list(map(lambda x: x.split(', '), actors_list))
+
+    # distionary with the number of times an actor appears in the list
+    actors_dic = {}
+    for list_actors in actors_list:
+        for actor in list_actors:
+            if(actor in actors_dic):
+                actors_dic[actor] += 1
+            else:
+                actors_dic[actor] = 1
+
+    '''
+    # List with all actors
+    actors = []
+    for element in actors_list:
+        actors += element
+    '''
+
+    keys = actors_dic.keys()
+    values = actors_dic.values()
+
+    actors_pre = {
+        'actors': keys,
+        'count': values
+    }
+
+    actors_df = pd.DataFrame(actors_pre)
+    max = int(actors_df['count'].max())
+
+    actors_df_filt = actors_df[actors_df['count'] == max]
+
+    actors = actors_df_filt['actors'].to_numpy().tolist()
+
+    return {
+        'actors': actors,
+        'appears': max 
+    }
 #---------------------------------------------------------------------------------#
 # Documentation used for queries
 '''
